b2f9e77bd4e82acfecd261ddd994aa946f804b5a
tatsuki-yamamoto <yama.tatsuki.0204@gmail.com>
2022-11-20 16:58:08 +0900

    fix: buildModules廃止

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,13 @@
 import { defineNuxtConfig } from 'nuxt/config';
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false, // SPAモード
-  runtimeConfig: {},
   build: {
     transpile: ['vuetify'],
   },
   css: ['vuetify/lib/styles/main.sass'],
-  buildModules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss'],
   vite: {
     define: {
       'process.env.DEBUG': 'false',
