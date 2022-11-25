32ecac1444b18822b69cb7384867327604627403
tatsuki-yamamoto <yama.tatsuki.0204@gmail.com>
2022-11-26 06:24:44 +0900

    refactor: ファイル名変更

--- components/templates/account/TemplatesAccountEditMain.vue
+++ components/templates/account/TemplatesAccountEditMain.vue
@@ -2,7 +2,7 @@
   <atoms-layout-container>
     <molecules-button-back></molecules-button-back>
     <atoms-text-title>アカウント編集</atoms-text-title>
-    <organisms-account-form></organisms-account-form>
-    <organisms-account-form-for-password></organisms-account-form-for-password>
+    <organisms-account-edit-form></organisms-account-edit-form>
+    <organisms-account-edit-form-for-password></organisms-account-edit-form-for-password>
   </atoms-layout-container>
 </template>
